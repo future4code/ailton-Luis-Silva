e77269cf317281c9c82867795c5e7965d05bf318
Luis Fernando <luisf.sil@hotmail.com>
2022-05-10 19:45:41 -0300

    aula4

--- template_javascript/index.js
+++ template_javascript/index.js
@@ -1,58 +1,116 @@
-// 
-
-// exercicio 1 
-// 10
-// 5
-
-// exercicio 2 
-// a 
-// c
-// b
-
-// exercicio 3
-
-// let p = prompt("Quantas horas você trabalha por dia?")
-// let t = prompt("Quanto você recebe por dia?")
-// alert(`Voce recebe ${t/p} por hora`)
-
-// let hora = prompt("Quantas horas você trabalha por dia?")
-// let dia = prompt("Quanto você recebe por dia?")
-// alert(`Voce recebe ${hora/dia} por hora`)
-
-//
 
 
-//
-
-// 1
-
-// const Name
-// const Idade 
-
-// typeof Name // string
-// typeof Idade  // number 
 
 
-// 2
+// //#1
 
-const pergunta1 = prompt("Você joga futebol?");
-console.log(pergunta1);
+// const bool1 = true
+// const bool2 = false
+// const bool3 = !bool2
 
-const pergunta2 = prompt("Você esta de azul hoje?");
-console.log(pergunta2);
+// let resultado = bool1 && bool2
+// console.log("a. ", resultado) // false
 
-const pergunta3 = prompt("Você gosta de chocolate?")
-console.log(pergunta3);
+// resultado = bool1 && bool2 && bool3 
+// console.log("b. ", resultado) // false 
 
-//3
+// resultado = !resultado && (bool1 || bool2) //true 
+// console.log("c. ", resultado) 
 
-let a = 10
-let b = 25
-
-a=b
-b=c
+// console.log("d. ", typeof resultado)
 
 
+
+// #2
+
+// let primeiroNumero = prompt("Digite um numero!")
+// let segundoNumero = prompt("Digite outro numero!")
+
+// const soma = primeiroNumero + segundoNumero
+
+// console.log(soma)
+
+// //ele só vai juntar os numeros, exemplo primeiro numero 10, segundo numero 20, ficaria 1020
+
+
+
+
+// //#3
+// let primeiroNumero = Number(prompt("Digite um numero!")); //faltava acrescentar Number//
+// let segundoNumero = Number(prompt("Digite outro numero!")); //faltava acrescentar Number//
+
+// const soma = primeiroNumero + segundoNumero
+
+// console.log(soma)
+
+
+
+
+//#1. Faça um programa que:
+    
+// a) Pergunte a idade do usuário
+    
+// b) Pergunte a idade do seu melhor amigo ou da sua melhor amiga
+
+// c) **Imprima na console** a seguinte mensagem: "Sua idade é maior do que a do seu melhor amigo?", seguido pela resposta (`true` ou `false`)
+
+// d) **Imprima na console** a diferença de idade (não tem problema se sair um número negativo)
+
+
+// let nomeUsuario = prompt("Qua sua idade?");
+// let idadeMelhorAmigo = prompt("Qual a idade do seu melhor amigo?");
+
+
+// const resultado = nomeUsuario <= idadeMelhorAmigo
+// console.log(resultado) 
+
+
+
+// // //#2 2. Faça um programa que:
+    
+//     a) Peça ao usuário que insira um número **par**
+    
+//     b) Imprima na console **o resto da divisão** desse número por 2.
+    
+//     c) Teste o programa com diversos números pares. Você notou um padrão? Escreva em um comentário de código.
+    
+//     d) O que acontece se o usuário inserir um número ímpar? Escreva em um comentário de código
+
+
+// let valor1 = prompt("insira um numero par")
+// console.log(valor1 % 2)
+
+// //quando par o resultado é 0 e quando impar o resultado é 1 
+
+// //#3 Faça um programa que pergunte ao usuário sua idade em anos. Depois, imprima no console 
+    
+//     a) A idade do usuário em meses
+    
+//     b) A idade do usuário em dias
+    
+// //     c) A idade do usuário em horas
+
+// const idadeAnos = prompt("qual a sua idade");
+// const idadeMeses = (idadeAnos * 12);
+// const idadeDias = (idadeAnos * idadeMeses * 365);
+// const idadeHoras = (idadeDias * 24);
+
+// console.log(idadeAnos);
+// console.log(idadeMeses);
+// console.log(idadeDias);
+// console.log(idadeHoras);
+
+
+// //#4 
+
+// // O primeiro numero é maior que segundo? true
+// O primeiro numero é igual ao segundo? false
+// O primeiro numero é divisível pelo segundo? true
+// O segundo numero é divisível pelo primeiro? true
+
+// obs: O true ou false vai depender dos números inseridos e do resultado das operações.
+
+//#4 travei 
 
 
 
