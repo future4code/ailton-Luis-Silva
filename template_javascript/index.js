19e5a5edbf3b1650e4f77e5e9c2fa214a93e6d85
Luis Fernando <luisf.sil@hotmail.com>
2022-05-09 21:46:02 -0300

    solucoes do trabalho

---
+++ template_javascript/index.js
@@ -0,0 +1,59 @@
+// 
+
+// exercicio 1 
+// 10
+// 5
+
+// exercicio 2 
+// a 
+// c
+// b
+
+// exercicio 3
+
+// let p = prompt("Quantas horas você trabalha por dia?")
+// let t = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${t/p} por hora`)
+
+// let hora = prompt("Quantas horas você trabalha por dia?")
+// let dia = prompt("Quanto você recebe por dia?")
+// alert(`Voce recebe ${hora/dia} por hora`)
+
+//
+
+
+//
+
+// 1
+
+// const Name
+// const Idade 
+
+// typeof Name // string
+// typeof Idade  // number 
+
+
+// 2
+
+const pergunta1 = prompt("Você joga futebol?");
+console.log(pergunta1);
+
+const pergunta2 = prompt("Você esta de azul hoje?");
+console.log(pergunta2);
+
+const pergunta3 = prompt("Você gosta de chocolate?")
+console.log(pergunta3);
+
+//3
+
+let a = 10
+let b = 25
+
+a=b
+b=c
+
+
+
+
+
+
